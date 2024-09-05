b26e142a6d38c0da28d2624d32992d653e757ab9
Derek Cooper <derekcooper20@gmail.com>
2024-09-04 23:00:49 -0600

    adds functions for list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react"
 import TodoCard from "./components/TodoCard"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
-
 function App() {
+  const [todos, setTodos] = useState([
+    "Go to the gym",
+    "Eat more fruits",
+    "Pick up medicine",
+  ])
+
+  const handleAddTodos = (newTodo) => {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+  const handleDeleteTodos = (index) => {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    setTodos(newTodoList)
+  }
+
+  const handleEditTodo = () => {}
   return (
     <>
-      <TodoInput />
-      <TodoList />
+      <TodoInput handleAddTodos={handleAddTodos} />
+      <TodoList handleDeleteTodos={handleDeleteTodos} todos={todos} />
       <TodoCard />
     </>
   )
